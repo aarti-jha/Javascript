85bbced69fd28208d596b6ed3fcf646c7efb7c06
aarti jha <bim77_15@bpcbirgunj.edu.np>
2023-10-15 21:16:38 +0545

    datatype added

--- variable.js
+++ variable.js
@@ -5,3 +5,14 @@ let nulls=null
 let undefineds 
 console.table([name,number,iSodd,nulls,undefineds])
 console.table([typeof(name),typeof(number),typeof(iSodd),typeof(nulls),typeof(undefineds) ])
+
+//datatype 
+//number => 2 to power 53
+//BIGINT 
+//string=> ""
+//boolean => false / true 
+// null => standard value 
+//undefined 
+// symbol =>unique
+// object 
+// 

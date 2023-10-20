99793c88f62557d2c300e920f4ca2dbb15146a67
aarti jha <bim77_15@bpcbirgunj.edu.np>
2023-10-20 10:46:41 +0545

    datatype and operator added

---
+++ operator.js
@@ -0,0 +1,32 @@
+//console.log(4+2) // 6
+//console.log(4-2)  // 2
+//console.log(4*2)  // 8
+//console.log(4/2) //2
+//console.log(4%2) // 0
+//console.log(4**2) // 16
+ let str1 = "hello"
+ let str2 = "world"
+ //console.log(str1+ " " +str2)  // hello world
+ //console.log("1" +2) //12
+ //console.log(1+ "2") //12
+ //console.log(1 + 2 +"3") //33
+ //console.log("1" + 2 + 3)//123
+
+ //console.log(+true) // 1
+ //console.log(+false) //0
+ //console.log(+"hello") //NaN
+
+ /*If used prefix, with operator before operand (for example, ++x), 
+ the increment operator increments and returns the value after incrementing.*/
+ let x = 3;
+ const y = ++x;
+console.log(`x=${x} , y=${y}`)// x is 4; y is 4
+
+/*If used postfix, with operator after operand (for example, x++), 
+the increment operator increments and returns the value before incrementing.*/
+let a= 3;
+ const b = a++;
+console.log(`a=${a} , b=${b}`)// a is 4; b is 3
+
+
+
--- variable.js
+++ variable.js
@@ -11,8 +11,37 @@ console.table([typeof(name),typeof(number),typeof(iSodd),typeof(nulls),typeof(un
 //BIGINT 
 //string=> ""
 //boolean => false / true 
-// null => standard value 
+// null => empty 
 //undefined 
 // symbol =>unique
 // object 
-// 
+   
+
+
+/*Return type of variables in JavaScript
+ Primitive Datatypes
+ Number => number
+String  => string
+Boolean  => boolean
+null  => object
+undefined  =>  undefined
+Symbol  =>  symbol
+BigInt  =>  bigint
+
+Non-primitive Datatypes
+Arrays  =>  object
+Function  =>  function
+Object  =>  object  
+
+*/
+
+let code = Symbol('43')
+let code1 =Symbol('43');
+console.log(code === code1);
+
+//JavaScript is a dynamically typed language. 
+//This means that variable types are determined at runtime,
+// and you do not need to explicitly declare the type of a variable before using it.
+// You can assign different types of values to a variable during its lifetime.
+//JavaScript's dynamic typing allows for
+// more flexibility but can lead to potential runtime errors if not handled carefully

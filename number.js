979c8622582463b9ba0974b5536347f019a370fb
aarti jha <bim77_15@bpcbirgunj.edu.np>
2023-10-26 10:13:08 +0545

    number and math methods are added

---
+++ number.js
@@ -0,0 +1,37 @@
+const num=new Number(100);
+const num1=100.567;
+const num2=10000000000000;
+
+//console.log(num);
+//console.log(num1);
+//console.log(num.valueOf())
+//console.log(num.toString());
+//console.log(num.toExponential(2));//1.00e+2
+//console.log(num.toFixed(2)); //100.00
+//console.log(num1.toPrecision(4));//100.6
+//console.log(num2.toLocaleString('en-In')); //1,00,00,00,00,00,000
+
+
+//console.log(Math.abs(-4));//4
+//console.log(Math.round(4.94));//5
+//console.log(Math.floor(4.94)); //4
+//console.log(Math.ceil(4.34)); //5
+//console.log(Math.min(6,3,8,9));//3
+//console.log(Math.max(6,3,8,9));//9
+//console.log(Math.random());
+//console.log(Math.round((Math.random()*10)+1));
+
+console.log(Math.pow(2,3));//8
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random() * (max - min) + min));
+
+console.log(Math.sqrt(36));
+
+
+
+
+
+
+

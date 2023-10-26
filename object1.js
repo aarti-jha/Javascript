4d970a19b3adb7af93398c1e4f44f172afdb53b0
aarti jha <bim77_15@bpcbirgunj.edu.np>
2023-10-26 21:06:07 +0545

    declaration and manipulation in object

---
+++ object1.js
@@ -0,0 +1,29 @@
+
+const sym=Symbol('key');
+const user ={
+    name:"kashish",
+    age:19,
+    Location:"Birgunj",
+    isStudent:true,
+    email:"kashish@gmail.com",
+    hobbies:["dancing", "singing"],
+    [sym]:"1234",
+    "number":9807776
+};
+
+//console.log(user);
+//console.log(user.Location);
+//console.log(user["Location"]);
+//console.log(user.number);
+//console.log(user["number"]);
+
+user.Location="Ithari"
+//Object.freeze(user); // cant change anything inside object.
+//user.Location="Ithari1"
+//console.log(user["Location"]);
+
+user.hello=function(){
+    console.log(`hello it's me ${this.name} from  ${this.Location} `)
+}
+
+console.log(user.hello());

c9b18fbb88a80422ae3d3fd2185ada2ca93fa4b4
aarti jha <bim77_15@bpcbirgunj.edu.np>
2023-10-26 14:27:25 +0545

    more functions of date and time are added

--- date_time.js
+++ date_time.js
@@ -10,4 +10,23 @@ let date = new Date();
 //console.log(date.toLocaleTimeString());//1:47:31 PM
 //console.log(date.toUTCString());//Thu, 26 Oct 2023 08:05:40 GMT
 
+const date1 = new Date(2023,0,24,5,34,59) // yy mm dd hh min sec
+//console.log(date1.toLocaleString());
 
+const date2 = new Date("01-14-2023")
+//console.log(date2.toLocaleString());
+//console.log(date2.getTime())
+//console.log(date2.getFullYear());
+
+const date3 = Date.now()
+//console.log(date3)
+
+console.log(date.toLocaleString())
+console.log(date.getTime());
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());

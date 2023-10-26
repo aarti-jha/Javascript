9a31b3cf0982e0583cf7016eb7ae8afdb0a54352
aarti jha <bim77_15@bpcbirgunj.edu.np>
2023-10-26 13:54:32 +0545

    date and time methods are added

---
+++ date_time.js
@@ -0,0 +1,13 @@
+let date = new Date();
+//console.log(date);//2023-10-26T08:02:31.898Z
+//console.log(date.toISOString());//2023-10-26T08:02:31.898Z
+//console.log(date.toString());//Thu Oct 26 2023 13:47:31 GMT+0545 (Nepal Time)
+//console.log(date.toJSON());//2023-10-26T08:02:31.898Z
+//console.log(date.toDateString());//Thu Oct 26 2023
+//console.log(date.toLocaleString());//10/26/2023, 1:47:31 PM
+//console.log(date.toLocaleDateString());//10/26/2023
+//console.log(date.toTimeString());//13:47:31 GMT+0545 (Nepal Time)
+//console.log(date.toLocaleTimeString());//1:47:31 PM
+//console.log(date.toUTCString());//Thu, 26 Oct 2023 08:05:40 GMT
+
+
